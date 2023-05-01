e31247334d3f3f9fb445c9e6b9f5d47ff0ebe477
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-05-01 15:32:46 +0300

    add 7/8

--- js/task-07.js
+++ js/task-07.js
@@ -2,5 +2,8 @@ const fontSizeInput = document.getElementById(
   "font-size-control"
 );
 
-fontSizeInput.addEventListener("input", (event) => )
+const textInput = document.getElementById("text");
 
+fontSizeInput.addEventListener("input", (event) => {
+  textInput.style.fontSize = `${event.currentTarget.value}px`;
+});
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,39 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleFormSubmit);
+
+// function handleFormSubmit(event) {
+//   event.preventDefault();
+//   const formElements = event.currentTarget.elements;
+
+//   if (
+//     formElements.email.value == "" ||
+//     formElements.password.value == ""
+//   ) {
+//     alert("Всі поля повинні бути заповнені");
+//   } else {
+//     const formDataObj = {};
+//     formDataObj.email = formElements.email.value;
+//     formDataObj.password = formElements.password.value;
+//     console.log(formDataObj);
+//     event.currentTarget.reset();
+//   }
+// }
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+
+  if (
+    formElements.email.value &&
+    formElements.password.value
+  ) {
+    const formDataObj = {};
+    formDataObj.email = formElements.email.value;
+    formDataObj.password = formElements.password.value;
+    console.log(formDataObj);
+    event.currentTarget.reset();
+  } else {
+    alert("Всі поля повинні бути заповнені");
+  }
+}
